9d5bcbf8a7f395e41a67a1f5ab180605b52950a7
Kaishiyoku <dmp@nfze.de>
2018-12-30 14:38:42 +0100

    added HTTP error handling

--- lib/bot.dart
+++ lib/bot.dart
@@ -27,11 +27,13 @@ class Bot {
       if (availableCommands.contains(adjustedMessage)) {
         final String uri = adjustedMessage.split(' ').length > 1 ? adjustedMessage.split(' ')[1] : '';
 
-        await http.get('https://astolfo.rocks/api/v1/images/random/' + uri, headers: {HttpHeaders.CONTENT_TYPE: "application/json"}).then((response) {
-
+        await http.get('https://astolfo.rocks/api/v1/images/random/${uri}', headers: {HttpHeaders.CONTENT_TYPE: "application/json"}).then((response) {
           var json = jsonDecode(response.body);
 
           event.message.reply('Rating: ${json['rating']}\nViews: ${json['views']}\n<http://unlimitedastolfo.works/post/view/${json['external_id']}>\n${json['url']}');
+        }).catchError((e) {
+          print('https://astolfo.rocks/api/v1/images/random/${uri}');
+          event.message.reply('There was an error getting an image of Astolfo :cry:');
         });
       }
     });
