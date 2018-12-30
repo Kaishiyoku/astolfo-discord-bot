dfc89882fb791bf0104704b9b82d2f4eccc3655e
Kaishiyoku <dmp@nfze.de>
2018-12-30 14:07:44 +0100

    removed console printout

--- lib/bot.dart
+++ lib/bot.dart
@@ -27,8 +27,6 @@ class Bot {
       if (availableCommands.contains(adjustedMessage)) {
         final String uri = adjustedMessage.split(' ').length > 1 ? adjustedMessage.split(' ')[1] : '';
 
-        print(uri);
-
         await http.get('https://astolfo.rocks/api/v1/images/random/' + uri, headers: {HttpHeaders.CONTENT_TYPE: "application/json"}).then((response) {
 
           var json = jsonDecode(response.body);
