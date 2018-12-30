16ca3790aa94760ee7acca1bedb36e01f5449fbe
Kaishiyoku <dmp@nfze.de>
2018-12-30 13:11:26 +0100

    added Dart structure

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:astolfo_discord_bot/bot.dart' as dart_example;
+
+main(List<String> arguments) {
+  
+}
---
+++ test/bot.dart
@@ -0,0 +1,8 @@
+import 'package:astolfo_discord_bot/bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+//  test('calculate', () {
+//    expect(calculate(), 42);
+//  });
+}
